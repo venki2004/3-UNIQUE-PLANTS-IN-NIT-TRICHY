cac5b3f77bde9a9dfbb22470d70cc1829c8142e6
Venkatesh <105818757+venki2004@users.noreply.github.com>
2022-09-02 12:44:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+               
+                display.innerText = eval(display.innerText);
+            
+                break;
+            case 'DEL':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
